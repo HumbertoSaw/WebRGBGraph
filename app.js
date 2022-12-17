a6e1f9083520fdd68d84cc96ebd81cec78f42b4e
HumbertoS <humbertosc321@gmail.com>
2022-12-17 13:47:47 -0500

    :poop: Image selector and crop kinda working. . .

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const imgLinkList = ["nzL5D6SX","NGCtpx1h","tgp8X1ht","RFPkpMJ1","HkqX55SX","9MV
 var cont = 0;
 var imageList = Array();
 
-var frame = document.querySelector('canvas');
-var ctx = frame.getContext('2d');
 var punterox = 0;
 var punteroy = 0;
 var punterow = 0;
 var punteroh = 0;
 
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext("2d");
+
+context.drawImage(imagen,0,0,canvas.width,canvas.height);
 fetch("json/json1.json").then((response) => {
   return response.json();
 })
@@ -33,77 +35,9 @@ function switchImage() {
     })
     .then((json) => jsonPrint.innerHTML = "Nombre: " + json.nombre + "<br>" + "Formato: " + json.formato + "<br>"+ "Peso: " + json.peso+ "<br>"+ "URL: " + json.url+ "<br>"+ "Res: " + json.res+ "<br>"+ "Descripción: " + json.des + "<br>"+ "Fecha: " + json.date);
 }
-/*
-imagen.addEventListener('click', function (e) {
-  const red = [];
-  const green = [];
-  const blue = [];
-  const alpha = [];
-  let ctx;
 
-  if(!this.canvas) {
-      this.canvas = document.createElement('canvas');
-      this.canvas.width = this.width;
-      this.canvas.height = this.height;
-      ctx=this.canvas.getContext('2d');
-      ctx.drawImage(this, 0, 0, this.width, this.height);
-  } else {
-    ctx=this.canvas.getContext('2d');
-  }
-  const pixel = ctx.getImageData(e.offsetX, e.offsetY, punterow, punteroh).data;
-  console.log(pixel);
-  console.log(pixel.length);
-  for(let i = 0; i < pixel.length; i++){
-    red[cont] = pixel[i];
-    i += 1;
-    green[cont] = pixel[i];
-    i += 1;
-    blue[cont] = pixel[i];
-    i += 1;
-    alpha[cont] = pixel[i];
-    cont += 1;
-  }
-  var traceRed = {
-    x: red,
-    type: 'histogram',
-    name: "Rojo",
-    marker: {
-      color: '#ff7c73',
-    },
-  };  
-  var traceGreen = {
-    x: green,
-    type: 'histogram',
-    name: "Verde",
-    marker: {
-    color: '#6ff77f',
-    },
-  };
-  var traceBlue = {
-    x: blue,
-    type: 'histogram',
-    name: "Azul",
-    marker: {
-      color: '#6fc7f7',
-    },
-  };
-  var layout = {
-    autosize: false,
-    width: 500,
-    height: 300,
-  };
-  
-  var data1 = [traceRed];
-  var data2 = [traceGreen];
-  var data3 = [traceBlue];
-
-  Plotly.newPlot('rojoHisto', data1, layout);
-  Plotly.newPlot('verdeHisto', data2, layout);
-  Plotly.newPlot('azulHisto', data3, layout);              
-});
-*/
 $(document).ready(function(){
-  $('#testImg').Jcrop({
+  $('#canvas').Jcrop({
     onSelect: function(c){
       punterox = (c.x);
       punteroy = (c.y);
@@ -120,8 +54,7 @@ $(document).ready(function(){
       const blue = [];
       const alpha = [];
 
-      ctx.drawImage(imagen,0,0);
-      const pixel = ctx.getImageData(punterox, punteroy, punterow, punteroh).data;
+      const pixel = context.getImageData(punterox, punteroy, punterow, punteroh).data;
       console.log("esto es pixel: "+pixel);
 
       for(let i = 0; i < pixel.length; i++){
@@ -134,8 +67,11 @@ $(document).ready(function(){
         alpha[cont] = pixel[i];
         cont += 1;
       }
+      var red0 = _.without(red, 0);
+      var green0 =_.without(green, 0);
+      var blue0 = _.without(blue, 0);
       var traceRed = {
-        x: red,
+        x: red0,
         type: 'histogram',
         name: "Rojo",
         marker: {
@@ -143,7 +79,7 @@ $(document).ready(function(){
         },
       };  
       var traceGreen = {
-        x: green,
+        x: green0,
         type: 'histogram',
         name: "Verde",
         marker: {
@@ -151,7 +87,7 @@ $(document).ready(function(){
         },
       };
       var traceBlue = {
-        x: blue,
+        x: blue0,
         type: 'histogram',
         name: "Azul",
         marker: {
